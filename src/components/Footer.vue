d36634532149bd31980bd6b16da9ec8cf4b8f7a3
Dandav2923 <daniele.emili23@gmail.com>
2022-01-14 14:50:39 +0100

    init

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,34 @@
 <template>
     <div class="footer">
+        <div class="container-footer-bis">
+        <div class="container-footer-top">
+
+        </div>
+        <div class="container-footer-bottom">
+            <ul class="container-list">
+                <li>
+                    <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                    <span>Digital Comics</span>
+                </li>
+                <li>
+                    <img src="../assets/img/buy-comics-merchandise.png" alt="">
+                    <span>Dc merchandise</span>
+                </li>
+                <li>
+                    <img class="small-img" src="../assets/img/buy-comics-shop-locator.png" alt="">
+                    <span>subscriptions</span>
+                </li>
+                <li>
+                    <img src="../assets/img/buy-comics-subscriptions.png" alt="">
+                    <span>Comic shop locator</span>
+                </li>
+                <li>
+                    <img src="../assets/img/buy-dc-power-visa.svg" alt="">
+                    <span>dc power visa</span>
+                </li>
+            </ul>
+        </div>
+    </div>
         <div class="container-footer">
             <div class="container-links">
                 <div class="container-left">
@@ -57,6 +86,43 @@
 
 <style lang="scss" scoped>
     @import "../assets/scss/partials/_variables.scss";
+    .container-footer-top {
+        height: 150px;
+        background-color: black;
+    }
+    .container-footer-bottom {
+        height: 200px;
+        background-color: $text-bg-color-blue;
+        .container-list{
+            display: flex;
+            list-style-type:none;
+            justify-content: center;
+            justify-content: space-around;
+            align-items: center;
+            width: 80%;
+            height: 100%;
+            margin: 0 auto;
+            li {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                color: white;
+                text-transform: uppercase;
+                img {
+                    width: 3em;
+                    height: auto;
+                }
+                .small-img {
+                    width: 2em;
+                    height: auto;
+                }
+                span {
+                    font-size: 0.8em;
+                    padding: 0.5em;
+                }
+            }
+        }
+    } 
     .footer {
         background-image: url(../assets/img/footer-bg.jpg);
         .container-footer {
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,32 +1,6 @@
 <template>
     <div class="container-main">
-        <div class="container-main-top">
-
-        </div>
-        <div class="container-main-bottom">
-            <ul class="container-list">
-                <li>
-                    <img src="../assets/img/buy-comics-digital-comics.png" alt="">
-                    <span>Digital Comics</span>
-                </li>
-                <li>
-                    <img src="../assets/img/buy-comics-merchandise.png" alt="">
-                    <span>Dc merchandise</span>
-                </li>
-                <li>
-                    <img class="small-img" src="../assets/img/buy-comics-shop-locator.png" alt="">
-                    <span>subscriptions</span>
-                </li>
-                <li>
-                    <img src="../assets/img/buy-comics-subscriptions.png" alt="">
-                    <span>Comic shop locator</span>
-                </li>
-                <li>
-                    <img src="../assets/img/buy-dc-power-visa.svg" alt="">
-                    <span>dc power visa</span>
-                </li>
-            </ul>
-        </div>
+        
     </div>
 </template> 
 <script>
@@ -36,42 +10,6 @@
     @import "../assets/scss/partials/_variables.scss";
     .container-main {
         height: 350px;
-    }
-    .container-main-top {
-        height: 150px;
         background-color: black;
     }
-    .container-main-bottom {
-        height: 200px;
-        background-color: $text-bg-color-blue;
-        .container-list{
-            display: flex;
-            list-style-type:none;
-            justify-content: center;
-            justify-content: space-around;
-            align-items: center;
-            width: 80%;
-            height: 100%;
-            margin: 0 auto;
-            li {
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                color: white;
-                text-transform: uppercase;
-                img {
-                    width: 3em;
-                    height: auto;
-                }
-                .small-img {
-                    width: 2em;
-                    height: auto;
-                }
-                span {
-                    font-size: 0.8em;
-                    padding: 0.5em;
-                }
-            }
-        }
-    } 
 </style>
